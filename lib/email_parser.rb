d07ac443acc9bcd3a0638b6a8437dcb87028046f
Matthew Wilson <wilsonmatt219@gmail.com>
2019-05-02 18:25:13 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,11 +10,11 @@ def initialize(csv_emails)
   @csv_emails = csv_emails
 end
 
-def parse
-  csv_emails.split.collect do |address|
-    address.split(',')
-  end
-  flatten.uniq
-end
-
+def parse 
+   csv_emails.split.collect do |address|
+     address.split(',') 
+   end
+   .flatten.uniq 
+ end 
+end 
 end
